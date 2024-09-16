92fa6bcf6a084d59f0c6a05f0c0ea9409ef8240b
rafael3007 <rafael.brito.1422@gmail.com>
2024-09-16 19:31:51 -0300

    Pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
 import { PageButton, PageButtonContainer } from "./styles";
 
 interface PaginationProps {
     pagesLength: number;
+    click: (index: number) => void;
+    activePage: number;
 }
 
 
-export default function Pagination({ pagesLength }: PaginationProps) {
+export default function Pagination({ pagesLength, click, activePage }: PaginationProps) {
     const pageNumbers = Array.from({ length: pagesLength }, (_, index) => index);
-    const [activePage, setAcitvePage] = useState(0);
-
-    function handleSetActivePage(index: number) {
-        setAcitvePage(index)
-    }
 
     return (
         <PageButtonContainer>
 
             {
-                pageNumbers.map(i => <PageButton key={"button-" + i.toString()} onClick={() => handleSetActivePage(i)} active={i === activePage}>{i + 1}</PageButton>)
+                pageNumbers.map(i => <PageButton key={"button-" + i.toString()} onClick={() => click(i)} active={i === activePage}>{i + 1}</PageButton>)
             }
         </PageButtonContainer>
     )
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,60 @@
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import Summary from "../../components/Summary";
 import SearchForm from "./components/SearchForm";
 import { FooterTransition, PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface TransactionsProps {
+    id: number,
+    description: string,
+    type: "outcome" | "income",
+    category: string,
+    price: number,
+    createdAt: string
+}
+
+
 export default function Transactions() {
+    const [transactions, setTransactions] = useState<Array<TransactionsProps>>([]);
+    const [transactionsVisibility, setTransactionsVisibility] = useState<Array<TransactionsProps>>([]);
+
+    const [pageIndex, setPageIndex] = useState<number>(0);
+    const INDEX_INIT = pageIndex * 5
+    const INDEX_FINAL = INDEX_INIT + 5
+
+
+    async function loadTransaction() {
+        const response = await fetch("http://localhost:3000/transactions")
+
+        const data: Array<TransactionsProps> = await response.json()
+
+
+
+        const dataShow = data.slice(INDEX_INIT, INDEX_FINAL)
+
+        setTransactions(data)
+        setTransactionsVisibility(dataShow)
+    }
+
+    function handleSetPageIndex(indexPage: number) {
+        setPageIndex(indexPage)
+    }
+
+    useEffect(() => {
+
+        loadTransaction()
+
+    }, [])
+
+    useEffect(() => {
+
+        const dataShow = transactions.slice(INDEX_INIT, INDEX_FINAL)
+
+        setTransactionsVisibility(dataShow)
+
+    }, [pageIndex])
+
     return (
         <div>
             <Header />
@@ -16,55 +66,24 @@ export default function Transactions() {
 
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td ><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
-                            <td >Venda</td>
-                            <td >13/19/2024</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td ><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight> </td>
-                            <td >Venda</td>
-                            <td >13/19/2024</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td ><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
-                            <td >Venda</td>
-                            <td >13/19/2024</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td ><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
-                            <td >Venda</td>
-                            <td >13/19/2024</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td ><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
-                            <td >Venda</td>
-                            <td >13/19/2024</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td ><PriceHighlight variant="outcome">- R$ 12.000,00</PriceHighlight></td>
-                            <td >Venda</td>
-                            <td >13/19/2024</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td ><PriceHighlight variant="outcome">- R$ 2.000,00</PriceHighlight></td>
-                            <td >Venda</td>
-                            <td >13/19/2024</td>
-                        </tr>
+                        {
+                            transactionsVisibility.map((transaction: TransactionsProps) => (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td ><PriceHighlight variant={transaction.type}>{transaction.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</PriceHighlight></td>
+                                    <td >{transaction.type}</td>
+                                    <td >{new Date(transaction.createdAt).toLocaleDateString('pt-BR')}</td>
+                                </tr>
+                            ))
+
+                        }
                     </tbody>
                 </TransactionsTable>
 
             </TransactionsContainer>
 
             <FooterTransition>
-                <Pagination pagesLength={3} />
+                {transactions.length > 0 ? <Pagination activePage={pageIndex} click={handleSetPageIndex} pagesLength={Math.floor(transactions.length / 5 + 1)} /> : ''}
             </FooterTransition>
         </div>
     )
